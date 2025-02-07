06d763b99ba5555f8e3527b6bbe2b02992c114fa
Mohanasomineni <mohanamanikanta7@gmail.com>
2025-02-07 12:32:06 -0500

    terraform

---
+++ locals/variables.tf
@@ -0,0 +1,18 @@
+variable "instance_type" {
+    type    = string
+    default = "t3.micro"
+
+}
+
+variable "project" {
+    default = "expense"
+}
+
+variable "component" {
+    default = "backend"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
