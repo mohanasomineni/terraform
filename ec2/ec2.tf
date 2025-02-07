1b4c91ace7605a185cead1795b01b50aac362de6
Mohanasomineni <mohanamanikanta7@gmail.com>
2025-02-07 00:15:54 -0500

    terraform

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "this" {
-    ami     = "ami-09c813fb71547fc4f"
+    ami     = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
     vpc_security_group_ids = [aws_security_group.allow_tls.id]
     instance_type = "t3.micro"
     tags = {
-        Name = "terraform-demo"
+        Name    = "terraform-demo"
+        Purpose = "terraform-practice"
     }
 }
 
